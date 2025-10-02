fa46d28116325eb6e8a91a24e1170ac2e8123a2e
unknown <vbruno383@gmail.com>
2025-10-02 16:19:34 -0300

    Instanciacao da base de dados

---
+++ src/main/java/com/simpli/workshopmongo/config/Instantiation.java
@@ -0,0 +1,30 @@
+package com.simpli.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.simpli.workshopmongo.domain.User;
+import com.simpli.workshopmongo.repository.UserRepository;
+
+@Configuration//para o spring entender que essa classe é uma configuração
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+			
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+	
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+
+}
