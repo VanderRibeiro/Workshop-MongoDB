f161c6e52a30a3b48c9d6a65b071370c40e6e108
unknown <vbruno383@gmail.com>
2025-10-03 00:02:04 -0300

    Delecao de usuario com DELETE

--- src/main/java/com/simpli/workshopmongo/resources/UserResource.java
+++ src/main/java/com/simpli/workshopmongo/resources/UserResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,17 +34,26 @@ public class UserResource {
 		return ResponseEntity.ok().body(listDto);
 	}
 
+	//Identificar
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {//pathvariable garante que o id do parametro seja o mesmo do map
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
 	
+	//Insert
 	@PostMapping
 	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {//Pega o body da requisição HTTP e converte a obj java como parametro
 		User obj = service.fromDTO(objDto);
 		obj = service.Insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();//cria um URL para o ID criado
 		return ResponseEntity.created(uri).build();
+	}
+	
+	//Delete
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable String id) {//pathvariable garante que o id do parametro seja o mesmo do map
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
