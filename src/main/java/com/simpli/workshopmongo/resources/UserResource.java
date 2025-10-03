b2c6e0c5ff01274bd83895eea720aa0922a2bad7
unknown <vbruno383@gmail.com>
2025-10-02 23:13:50 -0300

    inserção POST user

--- src/main/java/com/simpli/workshopmongo/resources/UserResource.java
+++ src/main/java/com/simpli/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.simpli.workshopmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.simpli.workshopmongo.domain.User;
 import com.simpli.workshopmongo.dto.UserDTO;
@@ -33,5 +37,13 @@ public class UserResource {
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {//pathvariable garante que o id do parametro seja o mesmo do map
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {//Pega o body da requisição HTTP e converte a obj java como parametro
+		User obj = service.fromDTO(objDto);
+		obj = service.Insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();//cria um URL para o ID criado
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/simpli/workshopmongo/services/UserService.java
+++ src/main/java/com/simpli/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.simpli.workshopmongo.domain.User;
+import com.simpli.workshopmongo.dto.UserDTO;
 import com.simpli.workshopmongo.repository.UserRepository;
 import com.simpli.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User Insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
